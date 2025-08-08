3afeab2363925eaec18a444e805c2c8caafff981
Pouya <pooya1361@gmail.com>
2025-08-08 13:45:44 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
